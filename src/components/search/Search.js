71e4f99209a89964da4f0d77b83de8b6293664f6
Eli Meschures <meschures.eli@gmail.com>
2018-08-21 10:46:58 -0700

    Finish Image Results and Search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
+import ImageResults from '../image-results/ImageResults';
 
 class Search extends Component {
   state = {
@@ -13,10 +15,27 @@ class Search extends Component {
   };
 
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
+    });
   };
 
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
+
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -40,6 +59,9 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
